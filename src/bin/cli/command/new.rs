f2b1721d40e88bfeaae8ed102108a99a126aadcf
Esteban Borai <estebanborai@gmail.com>
2024-07-15 23:03:33 -0400

    feat: support human expressions

--- src/bin/cli/command/new.rs
+++ src/bin/cli/command/new.rs
@@ -1,8 +1,9 @@
 use clap::Parser;
+use humantime::Duration;
 
 #[derive(Debug, Parser)]
 pub struct NewOpt {
-    /// Duration of the timer in seconds
-    #[clap(short = 'd', long = "duration")]
-    pub duration: u64,
+    /// Duration of the timer using time expressions (e.g. 1h30m, 1h, 30m, 1h30m30s, 1h30s, 30m30s, 1h30m30s)
+    #[clap(value_parser=clap::value_parser!(humantime::Duration))]
+    pub duration: Duration,
 }
